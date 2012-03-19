c6eeda334b21e88d15650f05e93b14fbd4a34973
Chris Banes <chris@senab.co.uk>
2012-03-19 17:09:47 +0000

    Optimize things, and add a new type of listener that doesn't fire every scroll event

--- library/src/com/dafruits/android/library/widgets/ExtendedListView.java
+++ library/src/com/dafruits/android/library/widgets/ExtendedListView.java
@@ -1,7 +1,5 @@
 package com.dafruits.android.library.widgets;
 
-import com.dafruits.android.library.R;
-
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -11,97 +9,87 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.ListView;
 
+import com.dafruits.android.library.R;
 
-public class ExtendedListView extends ListView {
-	
+public class ExtendedListView extends ListView implements OnScrollListener {
+
+	public static interface OnPositionChangedListener {
+
+		public void onPositionChanged(int position, View scrollBarPanel);
+
+	}
+
 	private OnScrollListener mOnScrollListener = null;
-	
+
 	private View mScrollBarPanel = null;
 	private int mScrollBarPanelPosition = 0;
 	private int mScrollBarPanelFadeDuration = 0;
-	
+
+	private OnPositionChangedListener mPositionChangedListener;
+	private int mFirstVisibleItemPosition = 0;
+
 	private Animation mInAnimation = null;
 	private Animation mOutAnimation = null;
-	
-	private Handler mHandler = new Handler();
-	private Runnable mScrollBarPanelFadeRunnable = new Runnable() {
-		
+
+	private final Handler mHandler = new Handler();
+
+	private final Runnable mScrollBarPanelFadeRunnable = new Runnable() {
+
 		@Override
 		public void run() {
 			mScrollBarPanel.startAnimation(mOutAnimation);
 		}
 	};
-	
+
 	/*
 	 * keep track of Measure Spec
 	 */
 	private int mWidthMeasureSpec;
 	private int mHeightMeasureSpec;
-	
-	
-	private OnScrollListener mScrollListener = new OnScrollListener() {
-		
-		@Override
-		public void onScrollStateChanged(AbsListView view, int scrollState) {
-			if (mOnScrollListener != null) {
-				mOnScrollListener.onScrollStateChanged(view, scrollState);
-			}
-		}
-		
-		@Override
-		public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-			updateScrollerView();
-			if (mOnScrollListener != null) {
-				mOnScrollListener.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
-			}
-		}
-	};
-	
-	
+
 	public ExtendedListView(Context context) {
-        this(context, null);
-    }
+		this(context, null);
+	}
 
-	
-    public ExtendedListView(Context context, AttributeSet attrs) {
-        this(context, attrs, android.R.attr.listViewStyle);
-    }
+	public ExtendedListView(Context context, AttributeSet attrs) {
+		this(context, attrs, android.R.attr.listViewStyle);
+	}
 
-    
-    public ExtendedListView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        
-    	super.setOnScrollListener(mScrollListener);
-        
-        final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ExtendedListView);
-        final int scrollBarPanelLayoutId = a.getResourceId(R.styleable.ExtendedListView_scrollBarPanel, -1);
-        a.recycle();
-        
-        if (scrollBarPanelLayoutId != -1) {
-        	setScrollBarPanel(scrollBarPanelLayoutId);
-        }
-		
-    	mScrollBarPanelFadeDuration = ViewConfiguration.getScrollBarFadeDuration() * 2;
-    	
-    	mInAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.in_animation);
-    	mOutAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.out_animation);
-    	
-    	mOutAnimation.setAnimationListener(new AnimationListener() {
-			
+	public ExtendedListView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+
+		super.setOnScrollListener(this);
+
+		final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ExtendedListView);
+		final int scrollBarPanelLayoutId = a.getResourceId(R.styleable.ExtendedListView_scrollBarPanel, -1);
+		a.recycle();
+
+		if (scrollBarPanelLayoutId != -1) {
+			setScrollBarPanel(scrollBarPanelLayoutId);
+		}
+
+		mScrollBarPanelFadeDuration = ViewConfiguration.getScrollBarFadeDuration() * 2;
+
+		mInAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.in_animation);
+		mOutAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.out_animation);
+
+		mOutAnimation.setAnimationListener(new AnimationListener() {
+
 			@Override
 			public void onAnimationStart(Animation animation) {
 			}
-			
+
 			@Override
 			public void onAnimationRepeat(Animation animation) {
 
 			}
-			
+
 			@Override
 			public void onAnimationEnd(Animation animation) {
 				if (mScrollBarPanel != null) {
@@ -109,106 +97,131 @@ public class ExtendedListView extends ListView {
 				}
 			}
 		});
-    }
+	}
 
-    
-    @Override
-    public void setOnScrollListener(OnScrollListener onScrollListener) {
-    	mOnScrollListener = onScrollListener;
-    }
-   
-    
-    public void setScrollBarPanel(View scrollBarPanel) {
-    	mScrollBarPanel = scrollBarPanel;
-    	mScrollBarPanel.setVisibility(View.GONE);
-    	requestLayout();
-    }
-    
-    
-    public void setScrollBarPanel(int resId) {
-    	setScrollBarPanel(LayoutInflater.from(getContext()).inflate(resId, this, false));
-    }
-    
-    
-    public View getScrollBarPanel() {
-    	return mScrollBarPanel;
-    }
-    
-    
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        
-        if (mScrollBarPanel != null && getAdapter() != null) {
-        	mWidthMeasureSpec = widthMeasureSpec;
-        	mHeightMeasureSpec = heightMeasureSpec;
-        	measureChild(mScrollBarPanel, widthMeasureSpec, heightMeasureSpec);
-        }
-    }
-    
-    
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-        
-        if (mScrollBarPanel != null) {
-        	final int x = getMeasuredWidth() - mScrollBarPanel.getMeasuredWidth() - getVerticalScrollbarWidth();
-        	mScrollBarPanel.layout(x, mScrollBarPanelPosition, x + mScrollBarPanel.getMeasuredWidth(), mScrollBarPanelPosition + mScrollBarPanel.getMeasuredHeight());
-        }
-    }
-    
-    
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-        super.dispatchDraw(canvas);
-    
-        if (mScrollBarPanel != null && mScrollBarPanel.getVisibility() == View.VISIBLE) {
-            drawChild(canvas, mScrollBarPanel, getDrawingTime());
-        }
-    }
-    
-    
-    @Override
-    public void onDetachedFromWindow() {
-    	super.onDetachedFromWindow();
-    	
-    	mHandler.removeCallbacks(mScrollBarPanelFadeRunnable);
-    }
-    
-    
-    private void updateScrollerView() {
-    	if (mScrollBarPanel == null) {
-    		return;
-    	}
-    	
-    	mHandler.removeCallbacks(mScrollBarPanelFadeRunnable);
-    	
-    	if (mScrollBarPanel.getVisibility() == View.GONE) {
-    		mScrollBarPanel.setVisibility(View.VISIBLE);
-    		mScrollBarPanel.startAnimation(mInAnimation);
-    	}
-    	
-    	/*
-    	 * from android source code (ScrollBarDrawable.java)
-    	 */
-    	final int thickness = getVerticalScrollbarWidth();
-    	int height = Math.round((float) getMeasuredHeight() * computeVerticalScrollExtent() / computeVerticalScrollRange());
-    	final int offset = Math.round((float) (getMeasuredHeight() - height) * computeVerticalScrollOffset() / (computeVerticalScrollRange() - computeVerticalScrollExtent()));
-    	final int minLength = thickness * 2;
-        if (height < minLength) {
-            height = minLength;
-        }
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		if (mOnScrollListener != null) {
+			mOnScrollListener.onScrollStateChanged(view, scrollState);
+		}
+	}
 
-        /*
-         * quick hack to handle TextView has ScrollBarPanel (to wrap text in case TextView's content has changed)
-         */
-        measureChild(mScrollBarPanel, mWidthMeasureSpec, mHeightMeasureSpec);
-        
-    	mScrollBarPanelPosition = offset + height / 2 - mScrollBarPanel.getMeasuredHeight() / 2;
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		updateScrollerView();
 
-    	final int x = getMeasuredWidth() - mScrollBarPanel.getMeasuredWidth() - getVerticalScrollbarWidth();
-    	mScrollBarPanel.layout(x, mScrollBarPanelPosition, x + mScrollBarPanel.getMeasuredWidth(), mScrollBarPanelPosition + mScrollBarPanel.getMeasuredHeight());
-    	
-    	mHandler.postDelayed(mScrollBarPanelFadeRunnable, mScrollBarPanelFadeDuration);
-    }
+		// If the position has changed, fire the OnPostionChangedListener
+		if (mFirstVisibleItemPosition != firstVisibleItem) {
+			mFirstVisibleItemPosition = firstVisibleItem;
+
+			if (null != mPositionChangedListener) {
+				mPositionChangedListener.onPositionChanged(firstVisibleItem, mScrollBarPanel);
+			}
+		}
+
+		if (mOnScrollListener != null) {
+			mOnScrollListener.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
+		}
+	}
+
+	public void setOnPositionChangedListener(OnPositionChangedListener onPositionChangedListener) {
+		mPositionChangedListener = onPositionChangedListener;
+	}
+
+	@Override
+	public void setOnScrollListener(OnScrollListener onScrollListener) {
+		mOnScrollListener = onScrollListener;
+	}
+
+	public void setScrollBarPanel(View scrollBarPanel) {
+		mScrollBarPanel = scrollBarPanel;
+		mScrollBarPanel.setVisibility(View.GONE);
+		requestLayout();
+	}
+
+	public void setScrollBarPanel(int resId) {
+		setScrollBarPanel(LayoutInflater.from(getContext()).inflate(resId, this, false));
+	}
+
+	public View getScrollBarPanel() {
+		return mScrollBarPanel;
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+		if (mScrollBarPanel != null && getAdapter() != null) {
+			mWidthMeasureSpec = widthMeasureSpec;
+			mHeightMeasureSpec = heightMeasureSpec;
+			measureChild(mScrollBarPanel, widthMeasureSpec, heightMeasureSpec);
+		}
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
+
+		if (mScrollBarPanel != null) {
+			final int x = getMeasuredWidth() - mScrollBarPanel.getMeasuredWidth() - getVerticalScrollbarWidth();
+			mScrollBarPanel.layout(x, mScrollBarPanelPosition, x + mScrollBarPanel.getMeasuredWidth(),
+					mScrollBarPanelPosition + mScrollBarPanel.getMeasuredHeight());
+		}
+	}
+
+	@Override
+	protected void dispatchDraw(Canvas canvas) {
+		super.dispatchDraw(canvas);
+
+		if (mScrollBarPanel != null && mScrollBarPanel.getVisibility() == View.VISIBLE) {
+			drawChild(canvas, mScrollBarPanel, getDrawingTime());
+		}
+	}
+
+	@Override
+	public void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+
+		mHandler.removeCallbacks(mScrollBarPanelFadeRunnable);
+	}
+
+	private void updateScrollerView() {
+		if (mScrollBarPanel == null) {
+			return;
+		}
+
+		mHandler.removeCallbacks(mScrollBarPanelFadeRunnable);
+
+		if (mScrollBarPanel.getVisibility() == View.GONE) {
+			mScrollBarPanel.setVisibility(View.VISIBLE);
+			mScrollBarPanel.startAnimation(mInAnimation);
+		}
+
+		/*
+		 * from android source code (ScrollBarDrawable.java)
+		 */
+		final int thickness = getVerticalScrollbarWidth();
+		int height = Math.round((float) getMeasuredHeight() * computeVerticalScrollExtent()
+				/ computeVerticalScrollRange());
+		final int offset = Math.round((float) (getMeasuredHeight() - height) * computeVerticalScrollOffset()
+				/ (computeVerticalScrollRange() - computeVerticalScrollExtent()));
+		final int minLength = thickness * 2;
+		if (height < minLength) {
+			height = minLength;
+		}
+
+		/*
+		 * quick hack to handle TextView has ScrollBarPanel (to wrap text in
+		 * case TextView's content has changed)
+		 */
+		measureChild(mScrollBarPanel, mWidthMeasureSpec, mHeightMeasureSpec);
+
+		mScrollBarPanelPosition = offset + height / 2 - mScrollBarPanel.getMeasuredHeight() / 2;
+
+		final int x = getMeasuredWidth() - mScrollBarPanel.getMeasuredWidth() - getVerticalScrollbarWidth();
+		mScrollBarPanel.layout(x, mScrollBarPanelPosition, x + mScrollBarPanel.getMeasuredWidth(),
+				mScrollBarPanelPosition + mScrollBarPanel.getMeasuredHeight());
+
+		mHandler.postDelayed(mScrollBarPanelFadeRunnable, mScrollBarPanelFadeDuration);
+	}
 }
